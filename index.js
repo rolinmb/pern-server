0887ba462aa12e3fc889e6aededc319f0622f2b1
Rolin Blake <rolinmblake@gmail.com>
2023-08-18 12:15:26 -0500

    small changes

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ app.delete("/todos/:id", async(req, res) => {
 });
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000")
+    console.log("server has started on port 5000");
 });
